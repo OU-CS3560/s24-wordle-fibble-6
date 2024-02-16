97054d5a587742e18fd2f541a6f1623f7c533b1d
maya-roediger <143741688+maya-roediger@users.noreply.github.com>
2024-02-16 11:13:30 -0500

    updated second 'app' folder to 'nerdle' for clarity; updated appropriate paths

--- app/src/main.server.ts
+++ app/src/main.server.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { AppComponent } from './nerdle/app.component';
+import { config } from './nerdle/app.config.server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './nerdle/app.config';
+import { AppComponent } from './nerdle/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
