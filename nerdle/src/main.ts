d52b8a4073c6d3cdee0b32801f77905a332c578a
joshaughnessy22 <js973521@ohio.edu>
2024-02-26 20:27:38 -0500

    Jade: successfully bootstrapped the AppComponent

--- nerdle/src/main.ts
+++ nerdle/src/main.ts
@@ -1,13 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { appConfig } from './app/app.config';
 import { AppModule } from './app/app.module';
 import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(moduleRef => {
-    // Bootstrap the AppComponent using the bootstrapApplication function
-    bootstrapApplication(AppComponent);
-  })
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent);
